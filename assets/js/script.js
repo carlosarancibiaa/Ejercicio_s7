753c70a7b71a747da48ea5081a89ef4c1edacdf3
Carlos Arancibia <carlos.arancibia.a@protonmail.com>
2023-04-13 01:05:02 -0400

    Inicio y término del ejercicio de comprobación de la sesión 7

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+let num1 = parseInt(prompt("Ingrese número 1"));
+let num2 = parseInt(prompt("Ingrese número 2"));
+
+if(num1>num2){
+    alert(num1+" es mayor que "+num2);
+}else if(num1<num2){
+    alert(num2+" es mayor que "+num1);
+}else{
+    alert(num1+" es igual a "+num2);
+}
